cc96b7b767f1adae2577de09ac2ac2890b7173c4
Bobbaru-K <bobbaru311@gmail.com>
2025-12-03 20:20:20 +0200

    feat(building-backrooms): update tables related files

--- next-app/src/features/building-backrooms/components/tables/columns.tsx
+++ next-app/src/features/building-backrooms/components/tables/columns.tsx
@@ -105,9 +105,9 @@ export const columns = ({
     enableHiding: true,
     enableSorting: true,
     enablePinning: true,
-    // size: 170,
-    // minSize: 170,
-    // maxSize: 200,
+    size: 185,
+    minSize: 185,
+    maxSize: 185,
     header: ({ column }) => {
       return (
         <THeadDropdown
@@ -123,7 +123,7 @@ export const columns = ({
       const date = getValue() as Date | null;
 
       return (
-        <div suppressHydrationWarning className="px-2">
+        <div suppressHydrationWarning className="px-2.5">
           {date
             ? dateFormatter({
                 date,
@@ -152,7 +152,7 @@ export const columns = ({
     minSize: 75,
     maxSize: 100,
     header: ({ column }) => (
-      <div className="grid place-items-center px-2">
+      <div className="grid place-items-center px-2.5">
         <THeadDropdown
           id="actions"
           label={"Actions"}
@@ -167,7 +167,7 @@ export const columns = ({
       const captainRole = row.original;
 
       return (
-        <div className="grid place-items-center px-2">
+        <div className="grid place-items-center px-2.5">
           <RowActions buildingBackroom={captainRole} />
         </div>
       );
